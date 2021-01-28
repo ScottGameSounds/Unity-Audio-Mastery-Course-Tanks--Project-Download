b57121213dbfa8b59cd51ac82b90628af8a927f8
Henry Scott | ScottGameSounds <64966356+ScottGameSounds@users.noreply.github.com>
2021-01-28 21:41:29 +0000

    Complete Project Push
    The complete Tanks! project with all of the

---
+++ TANKS! Blank Project/Assets/TANKS!/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Complete
+{
+    public class PowerUp : MonoBehaviour
+    {
+        public GameObject spawnParticles;
+        public GameObject MeshObject;
+
+        public float effectTime = 10f;
+        public float destroyObjectDelay = 2f;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == 9)
+            {
+                PowerUpEffect(other);
+            }
+        }
+
+        void PowerUpEffect(Collider player)
+        {
+            Instantiate(spawnParticles, transform.position, transform.rotation);
+
+            TankShooting Tank = player.GetComponent<TankShooting>();
+            Tank.applyBonusPower = true;
+            Tank.effectTime = effectTime;
+            Tank.Timer = effectTime;
+
+            GetComponent<SphereCollider>().enabled = false;
+            MeshObject.SetActive(false);
+
+            Destroy(gameObject, destroyObjectDelay);
+        }
+    }
+}
+
---
+++ TANKS! Blank Project/Assets/TANKS!/Scripts/PowerUps/RotatePowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePowerUp : MonoBehaviour
+{
+    private Vector3 startPos;
+    public float amplitude = 1f;
+    public float rotationSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        transform.Rotate(new Vector3(0f, Random.Range(0f, 360f), 0f)); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.deltaTime;
+
+        float x = transform.position.x;
+        float y = (startPos.y + Mathf.Sin(Time.time)) * amplitude;
+        float z = transform.position.z;
+        transform.Rotate(new Vector3(0f, rotationSpeed, 0f));
+        transform.position = new Vector3(x, y, z);
+    }
+}
